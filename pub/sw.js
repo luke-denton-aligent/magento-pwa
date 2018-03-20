8acb21f6da6b42d191d013e3138cf02fb3039b20
Luke Denton <luke.denton@alig1604.int.aligent.consulting>
2018-03-20 14:00:51 +1030

    Service worker will now persist cart across tabs Both adding and removing items

--- pub/sw.js
+++ pub/sw.js
@@ -1,11 +1,8 @@
-function handleMessage(data, clientId) {
+function handleMessage(clientId) {
     clients.matchAll().then(clients => {
         clients.forEach(client => {
-
-            // No need to update the tab that
-            // sent the data
+            // No need to update the tab that sent the data
             if (client.id !== clientId) {
-                console.log('[ServiceWorker] Posting message to other clients to update cart');
                 client.postMessage('update_cart');
             }
 
@@ -18,18 +15,12 @@ self.addEventListener('install', event => {
     self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-    //console.log("Activate event")
-});
-
 self.addEventListener('message', function(event){
     console.log('[ServiceWorker] Received message');
-    // Receive the data from the client
-    var data = event.data;
 
     // The unique ID of the tab
     var clientId = event.source.id;
 
     // A function that handles the message
-    handleMessage(data, clientId);
+    handleMessage(clientId);
 });
