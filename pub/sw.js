74f23877099277f2dffc39966c8038f212160ca2
Luke Denton <luke.denton@alig1604.int.aligent.consulting>
2018-03-20 13:15:53 +1030

    Cart now updates across tabs, sort of When the cart is updated, a message is posted to the service worker, asking it to update the cart in all open tabs. Currently this will update with the cart in the previous state, because the message is sent, and the cart in the other tab updated, before the request for the current tab is even finished

--- pub/sw.js
+++ pub/sw.js
@@ -1,16 +1,35 @@
-console.log('Working');
+function handleMessage(data, clientId) {
+    clients.matchAll().then(clients => {
+        clients.forEach(client => {
 
-function syncTabState() {
+            // No need to update the tab that
+            // sent the data
+            if (client.id !== clientId) {
+                console.log('[ServiceWorker] Posting message to other clients to update cart');
+                client.postMessage('update_cart');
+            }
 
+        })
+    })
 }
 
+self.addEventListener('install', event => {
+    // The promise that skipWaiting() returns can be safely ignored.
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    //console.log("Activate event")
+});
+
 self.addEventListener('message', function(event){
+    console.log('[ServiceWorker] Received message');
     // Receive the data from the client
     var data = event.data;
 
     // The unique ID of the tab
-    var clientId = event.source.id
+    var clientId = event.source.id;
 
     // A function that handles the message
-    self.syncTabState(data, clientId);
+    handleMessage(data, clientId);
 });
